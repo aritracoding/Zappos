8f4fb4040f21fbf088357b86b2b6ba0c89952834
AritraDe <80158103+aritracoding@users.noreply.github.com>
2022-02-26 05:17:34 +0530

    add to cart part design added

--- addToCart.js
+++ addToCart.js
@@ -1,118 +1,130 @@
-var data =[
-    {
-        image_url: "https://m.media-amazon.com/images/I/711luD-V41L._AC_SR920,736_.jpg"
-    },
-    {
-        image_url: "https://m.media-amazon.com/images/I/71oxMcVJSLL._AC_SR920,736_.jpg"
-    },
-    {
-        image_url: "https://m.media-amazon.com/images/I/61fxl5D6DBL._AC_SR920,736_.jpg"
-    },
-    {
-        image_url: "https://m.media-amazon.com/images/I/71ZmPUMmSGL._AC_SR920,736_.jpg"
-    },
-    {
-        image_url: "https://m.media-amazon.com/images/I/71ZmPUMmSGL._AC_SR920,736_.jpg"
-    },
-    {
-        image_url: "https://m.media-amazon.com/images/I/61BarDq5quL._AC_SR920,736_.jpg"
-    },
-    // {
-    //     image_url: ""
-    // },
+// var data =[
+//     {
+//         image_url: "https://m.media-amazon.com/images/I/711luD-V41L._AC_SR920,736_.jpg"
+//     },
+//     {
+//         image_url: "https://m.media-amazon.com/images/I/71oxMcVJSLL._AC_SR920,736_.jpg"
+//     },
+//     {
+//         image_url: "https://m.media-amazon.com/images/I/61fxl5D6DBL._AC_SR920,736_.jpg"
+//     },
+//     {
+//         image_url: "https://m.media-amazon.com/images/I/71ZmPUMmSGL._AC_SR920,736_.jpg"
+//     },
+//     {
+//         image_url: "https://m.media-amazon.com/images/I/71ZmPUMmSGL._AC_SR920,736_.jpg"
+//     },
+//     {
+//         image_url: "https://m.media-amazon.com/images/I/61BarDq5quL._AC_SR920,736_.jpg"
+//     },
+//     // {
+//     //     image_url: ""
+//     // },
     
-]
+// ]
 
 
-var data1 = [
-    {
-        img_url: "https://m.media-amazon.com/images/I/71-toOBbozS._AC_SX272_.jpg",
-        title1: "Salomon",
-        title2: "Sense Ride 4",
-        price: "$119.95",
-    },
-    {
-        img_url: "https://m.media-amazon.com/images/I/816pliBEJ3L._AC_SX272_.jpg",
-        title1: "Salomon",
-        title2: "Pulsar",
-        price: "$179.95",
-    },
-    {
-        img_url: "https://m.media-amazon.com/images/I/71uYOW+D2BL._AC_SX272_.jpg",
-        title1: "Salomon",
-        title2: "Wildcross 2",
-        price: "$129.95",
-    },
-    {
-        img_url: "https://m.media-amazon.com/images/I/71OqhwGRV0L._AC_SX272_.jpg",
-        title1: "Salomon",
-        title2: "Sonic 4",
-        price: "$129.95",
-    },
-    {
-        img_url: "https://m.media-amazon.com/images/I/61X434cML4L._AC_SX272_.jpg",
-        title1: "Salomon",
-        title2: "Predict 2",
-        price: "$149.95",
-    },
-    {
-        img_url: "https://m.media-amazon.com/images/I/81XFVKSYuRL._AC_SX272_.jpg",
-        title1: "Merrell",
-        title2: "Moab Flight",
-        price: "$119.95",
-    },
-    {
-        img_url: "https://m.media-amazon.com/images/I/8106ru8ubQL._AC_SX272_.jpg",
-        title1: "Brooks",
-        title2: "Catamount",
-        price: "$159.95",
-    },
-    {
-        img_url: "https://m.media-amazon.com/images/I/71r86H2Iw4L._AC_SX272_.jpg",
-        title1: "ASICS",
-        title2: "Fuji Lite 2",
-        price: "$119.95",
-    },
-    {
-        img_url: "https://m.media-amazon.com/images/I/71hsORsmYqL._AC_SX272_.jpg",
-        title1: "Hoka",
-        title2: "Speedgoat 4",
-        price: "$144.95",
-    },
-    {
-        img_url: "https://m.media-amazon.com/images/I/81NCdrpxOHL._AC_SX272_.jpg",
-        title1: "Salomon",
-        title2: "Ultra Glide",
-        price: "$139.95",
-    },
-    {
-        img_url: "https://m.media-amazon.com/images/I/71Fg1D0MkpL._AC_SX272_.jpg",
-        title1: "Hoka",
-        title2: "Clifton 8",
-        price: "$140.95",
-    },
-    {
-        img_url: "https://m.media-amazon.com/images/I/71BPwdWiGiS._AC_SX272_.jpg",
-        title1: "Salomon",
-        title2: "XA Wild",
-        price: "$129.95",
-    },
-    {
-        img_url: "https://m.media-amazon.com/images/I/81NELer3a2L._AC_SX272_.jpg",
-        title1: "Altra",
-        title2: "Lone Peak 5",
-        price: "$129.95",
-    },
-    {
-        img_url: "https://m.media-amazon.com/images/I/71y71WPOxDL._AC_SX272_.jpg",
-        title1: "Salomon",
-        title2: "Sonic 4 Balance",
-        price: "$129.95",
-    },
-    {
-        img_url: "https://m.media-amazon.com/images/I/81iKeiSdloL._AC_SX272_.jpg",
-        title1: "Salomon",
-        title2: "Peregrine 11",
-        price: "$94.95",
-    },
-]
+// var data1 = [
+//     {
+//         img_url: "https://m.media-amazon.com/images/I/71-toOBbozS._AC_SX272_.jpg",
+//         title1: "Salomon",
+//         title2: "Sense Ride 4",
+//         price: "$119.95",
+//     },
+//     {
+//         img_url: "https://m.media-amazon.com/images/I/816pliBEJ3L._AC_SX272_.jpg",
+//         title1: "Salomon",
+//         title2: "Pulsar",
+//         price: "$179.95",
+//     },
+//     {
+//         img_url: "https://m.media-amazon.com/images/I/71uYOW+D2BL._AC_SX272_.jpg",
+//         title1: "Salomon",
+//         title2: "Wildcross 2",
+//         price: "$129.95",
+//     },
+//     {
+//         img_url: "https://m.media-amazon.com/images/I/71OqhwGRV0L._AC_SX272_.jpg",
+//         title1: "Salomon",
+//         title2: "Sonic 4",
+//         price: "$129.95",
+//     },
+//     {
+//         img_url: "https://m.media-amazon.com/images/I/61X434cML4L._AC_SX272_.jpg",
+//         title1: "Salomon",
+//         title2: "Predict 2",
+//         price: "$149.95",
+//     },
+
+
+
+
+
+
+//     {
+//         img_url: "https://m.media-amazon.com/images/I/81XFVKSYuRL._AC_SX272_.jpg",
+//         title1: "Merrell",
+//         title2: "Moab Flight",
+//         price: "$119.95",
+//     },
+//     {
+//         img_url: "https://m.media-amazon.com/images/I/8106ru8ubQL._AC_SX272_.jpg",
+//         title1: "Brooks",
+//         title2: "Catamount",
+//         price: "$159.95",
+//     },
+//     {
+//         img_url: "https://m.media-amazon.com/images/I/71r86H2Iw4L._AC_SX272_.jpg",
+//         title1: "ASICS",
+//         title2: "Fuji Lite 2",
+//         price: "$119.95",
+//     },
+//     {
+//         img_url: "https://m.media-amazon.com/images/I/71hsORsmYqL._AC_SX272_.jpg",
+//         title1: "Hoka",
+//         title2: "Speedgoat 4",
+//         price: "$144.95",
+//     },
+//     {
+//         img_url: "https://m.media-amazon.com/images/I/81NCdrpxOHL._AC_SX272_.jpg",
+//         title1: "Salomon",
+//         title2: "Ultra Glide",
+//         price: "$139.95",
+//     },
+
+
+
+
+
+//     {
+//         img_url: "https://m.media-amazon.com/images/I/71Fg1D0MkpL._AC_SX272_.jpg",
+//         title1: "Hoka",
+//         title2: "Clifton 8",
+//         price: "$140.95",
+//     },
+//     {
+//         img_url: "https://m.media-amazon.com/images/I/71BPwdWiGiS._AC_SX272_.jpg",
+//         title1: "Salomon",
+//         title2: "XA Wild",
+//         price: "$129.95",
+//     },
+//     {
+//         img_url: "https://m.media-amazon.com/images/I/81NELer3a2L._AC_SX272_.jpg",
+//         title1: "Altra",
+//         title2: "Lone Peak 5",
+//         price: "$129.95",
+//     },
+//     {
+//         img_url: "https://m.media-amazon.com/images/I/71y71WPOxDL._AC_SX272_.jpg",
+//         title1: "Salomon",
+//         title2: "Sonic 4 Balance",
+//         price: "$129.95",
+//     },
+//     {
+//         img_url: "https://m.media-amazon.com/images/I/81iKeiSdloL._AC_SX272_.jpg",
+//         title1: "Salomon",
+//         title2: "Peregrine 11",
+//         price: "$94.95",
+//     },
+// ]
+
