64d36e5f69cb6f07d4041fbe70c38513f9419b50
AritraDe <80158103+aritracoding@users.noreply.github.com>
2022-02-25 14:23:44 +0530

    image added in add to cart

---
+++ addToCart.js
@@ -0,0 +1,24 @@
+var data =[
+    {
+        image_url: "https://m.media-amazon.com/images/I/711luD-V41L._AC_SR920,736_.jpg"
+    },
+    {
+        image_url: "https://m.media-amazon.com/images/I/71oxMcVJSLL._AC_SR920,736_.jpg"
+    },
+    {
+        image_url: "https://m.media-amazon.com/images/I/61fxl5D6DBL._AC_SR920,736_.jpg"
+    },
+    {
+        image_url: "https://m.media-amazon.com/images/I/71ZmPUMmSGL._AC_SR920,736_.jpg"
+    },
+    {
+        image_url: "https://m.media-amazon.com/images/I/71ZmPUMmSGL._AC_SR920,736_.jpg"
+    },
+    {
+        image_url: "https://m.media-amazon.com/images/I/61BarDq5quL._AC_SR920,736_.jpg"
+    },
+    // {
+    //     image_url: ""
+    // },
+    
+]
